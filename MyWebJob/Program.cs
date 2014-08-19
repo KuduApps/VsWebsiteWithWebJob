f3bfa7bd9d06061ce2aa3f4d5f112764915c525f
Amit Apple <amitap@microsoft.com>
2014-08-19 15:57:47 -0700

    vs tooling website and webjobs

---
+++ MyWebJob/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace MyWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Working");
+                Thread.Sleep(60*1000);
+            }
+        }
+    }
+}
